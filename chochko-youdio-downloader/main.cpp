bed64e3e26114179dd66df51b7b0d3020ece9918
RedCuckoo <momotenko.yuriy@gmail.com>
2020-03-07 18:55:25 +0200

    Raw variant

--- chochko-youdio-downloader/main.cpp
+++ chochko-youdio-downloader/main.cpp
@@ -1,11 +1,12 @@
 #include "MainWindow.h"
 #include "DiscoverWindow.h"
+#include "UrlWindow.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	DiscoverWindow w;
+	UrlWindow w;
 	w.show();
 	return a.exec();
 }
